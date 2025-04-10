d0448906534abeec7f295f61cee410d2911f836f
Vinz <abc@xyz.com>
2025-04-10 11:28:39 +0100

    navbar/footer/routedpages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,24 @@
-function App() {
+import { Routes, Route  } from "react-router-dom";
+import MainLayout from "./layout/MainLayout";
+import Home from "./pages/Home";
+import Government from "./pages/Government";
+import Services from "./pages/Services";
+import News from "./pages/News";
 
-  return <>
-  <h1>HELLO WORLD</h1>
-  </>;
+const App = () => {
+return (
+<Routes>
+    <Route element={<MainLayout />}>
+    <Route index element={<Home />} />
+    <Route path="/government" element={<Government />} />
+    <Route path="/services" element={<Services />} />
+    <Route path="/news" element={<News />} />
+    </Route>
+  </Routes>
+)
+}
    
   
-}
+
 
 export default App
---
+++ frontend/src/layout/MainLayout.tsx
@@ -0,0 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Navbar from '../components/navbar/Navbar';
+import Footer from '../components/footer/Footer';
+
+const MainLayout = () => (
+  <>
+    <Navbar />
+    <main style={{ padding: '2rem', minHeight: '80vh' }}>
+      <Outlet />
+    </main>
+    <Footer />
+  </>
+);
+
+export default MainLayout;
