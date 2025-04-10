21f69065522502c3ac69c55cae748e403039e96f
Vinz <abc@xyz.com>
2025-04-10 19:19:47 +0100

    style navbar

---
+++ frontend/src/components/sections/Hero.tsx
@@ -0,0 +1,14 @@
+import styles from './Hero.module.css';
+
+export const Hero = () => {
+  return (
+    
+      <div className={styles.heroContent}>
+        <h1 className={styles.heroHeader}>Welcome to CivicLink Portal</h1>
+        <p>Access services, find information, and connect with government.</p>
+        <input type="text" placeholder="Search for services (e.g., government officials, passports, taxes, permits)..." />
+        <button className={styles.btn}>Search</button>
+      </div>
+    
+  );
+}
