a64cb33161d1a920c07a26e25d64a55e65c040aa
Vinz <abc@xyz.com>
2025-04-11 11:13:12 +0100

    imported the styles for navbar

--- frontend/src/layout/MainLayout.tsx
+++ frontend/src/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/navbar/Navbar';
 import Footer from '../components/footer/Footer';
+import '../components/navbar/Navbar.module.css'
 
 const MainLayout = () => (
   <>
