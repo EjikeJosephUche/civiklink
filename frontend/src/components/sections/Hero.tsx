f25a5cb339320dd6ea317f0740e8d50daff3c79c
Vinz <abc@xyz.com>
2025-04-12 14:18:47 +0100

    hompage by Mario

--- frontend/src/components/sections/Hero.tsx
+++ frontend/src/components/sections/Hero.tsx
@@ -1,15 +1,47 @@
-// import styles from './Hero.module.css';
+// import React from "react";
+import styles from '../../styles/homePage.module.css';
 
 export const Hero = () => {
   return (
-    <></>
-      // <div className={styles.heroContents}>
-      //   <div className={styles.overlay}></div>
-      //   <h1 className={styles.heroHeader}>Welcome to CivicLink Portal</h1>
-      //   <p>Access services, find information, and connect with government.</p>
-      //   <input type="text" placeholder="Search for services (e.g., government officials, passports, taxes, permits)..." />
-      //   <button className={styles.btn}>Search</button>
-      // </div>
-    
+    <section className={styles.heroSection}>
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          backgroundSize: "cover",
+          backgroundImage: "url('images/hero.png')",
+        }}
+      >
+        <h1 className={styles.heroTitle}>Welcome to CivicLink Portal</h1>
+        <p className={styles.heroSubtitle}>
+          Access services, find information, and connect with government.
+        </p>
+        <div className={styles.searchBar}>
+          <input
+            type="text"
+            placeholder="Search for services (e.g., government officials, passports, taxes, permits)..."
+            className={styles.searchInput}
+          />
+          <button className={styles.searchBotton}>Search</button>
+        </div>
+      </div>
+    </section>
   );
-}
+};
+
+
+// // import styles from './Hero.module.css';
+
+// export const Hero = () => {
+//   return (
+//     <></>
+//       // <div className={styles.heroContents}>
+//       //   <div className={styles.overlay}></div>
+//       //   <h1 className={styles.heroHeader}>Welcome to CivicLink Portal</h1>
+//       //   <p>Access services, find information, and connect with government.</p>
+//       //   <input type="text" placeholder="Search for services (e.g., government officials, passports, taxes, permits)..." />
+//       //   <button className={styles.btn}>Search</button>
+//       // </div>
+    
+//   );
+// }
